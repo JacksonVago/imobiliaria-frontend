ea8e532e74ce378bc028e6fac071fda7c56b9736
JacksonVago <jackson@natividadesolucoes.com.br>
2025-09-16 16:33:52 -0300

    build

--- src/pages/main/colaboradores/index.tsx
+++ src/pages/main/colaboradores/index.tsx
@@ -112,10 +112,10 @@ enum QueryKeys {
 }
 
 import { PageLoader } from '@/pages/assistant/page-loader'
-import { z } from 'zod'
+//import { z } from 'zod'
 import axios from 'axios'
 
-const createUserSchema = z.object({
+/*const createUserSchema = z.object({
   name: z.string().min(1, 'O nome é obrigatório.'),
   email: z.string().email('Insira um e-mail válido.'),
   password: z.string().min(6, 'A senha deve ter pelo menos 6 caracteres.')
@@ -146,7 +146,7 @@ const updateUserSchema = z.object({
       'VIEW_LOCACOES'
     ])
   )
-})
+})*/
 
 export const ListarColaboradores = () => {
   const [selectedUser, setSelectedUser] = React.useState<User | null>(null)
